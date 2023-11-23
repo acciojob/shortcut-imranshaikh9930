4d0f375cd34870ef091b4becdaa0b8f3d308dd9f
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-23 13:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	  if(s1.length === 0 || s2.length === 0){
+        return "";
+    }
+
+    let st1 = s1.slice(0,1);
+    let st2 = s2.slice(0,1);
+    
+   return st1+st2;
 }
 
 // Do not change the code below.
